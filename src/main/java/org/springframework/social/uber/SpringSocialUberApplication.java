f1b05275ab533fe9b919d4fe0e8d9b7756265bd6
kolyan <vkont4@gmail.com>
2018-04-06 11:26:32 +0300

    init

---
+++ src/main/java/org/springframework/social/uber/SpringSocialUberApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.social.uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialUberApplication.class, args);
+	}
+}
